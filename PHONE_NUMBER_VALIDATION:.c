9a9c57e034ceb57f934bbbdefb7190e6183685be
nandana2005 <119730215+nandana2005@users.noreply.github.com>
2023-01-26 17:00:31 +0530

    #include<stdio.h> int main() {     int i,j=0,t;     scanf("%d",&i);     t=i;     while(i>0)     {         i=i/10;         j++;         if(j==10&&i==0)         {             j--;         }     }     if(j==9)     {         printf("Valid");     }     else     {         printf("Invalid");     } }

---
+++ PHONE_NUMBER_VALIDATION:.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int i,j=0,t;
+    scanf("%d",&i);
+    t=i;
+    while(i>0)
+    {
+        i=i/10;
+        j++;
+        if(j==10&&i==0)
+        {
+            j--;
+        }
+    }
+    if(j==9)
+    {
+        printf("Valid");
+    }
+    else
+    {
+        printf("Invalid");
+    }
+}
