aaf12186bb8744809fc5ac841b3d8a936f87bc52
nandana2005 <119730215+nandana2005@users.noreply.github.com>
2023-01-26 20:40:47 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,rem,rev=0,a,sa,sr,rs=0;     scanf("%d",&n);     a=n;     while(n>0)     {         rem=n%10;         rev=rev*10+rem;         n=n/10;     }     sa=pow(a,2);     sr=pow(rev,2);     while(sr>0)     {         rem=sr%10;         rs=rs*10+rem;         sr=sr/10;     }     if(sa==rs)     {         printf("True");     }     else     {         printf("False");     } }

--- Adam_number.c
+++ Adam_number.c
@@ -1,23 +1,25 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
-    int n,res1=0,res2=0,res3=0,res4=0,d;
+    int n,rem,rev=0,a,sa,sr,rs=0;
     scanf("%d",&n);
-    res1=n*n;
-    while(n)
+    a=n;
+    while(n>0)
     {
-        d=n%10;
-        res2=res2*10+d;
+        rem=n%10;
+        rev=rev*10+rem;
         n=n/10;
     }
-    res3=res2*res2;
-    while(res3)
+    sa=pow(a,2);
+    sr=pow(rev,2);
+    while(sr>0)
     {
-        d=res3%10;
-        res4=res4*10+d;
-        res3=res3/10;
+        rem=sr%10;
+        rs=rs*10+rem;
+        sr=sr/10;
     }
-    if(res1==res4)
+    if(sa==rs)
     {
         printf("True");
     }
