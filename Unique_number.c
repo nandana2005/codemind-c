3ae17065b2e7908411b7ecbf47fb93370b03f227
nandana2005 <119730215+nandana2005@users.noreply.github.com>
2023-01-26 17:32:24 +0530

    #include<stdio.h> int main() {     int n,arr[100]={0},d;     scanf("%d",&n);     while(n)     {         d=n%10;         if(arr[d]==1)         {             break;         }         else         {             arr[d]=1;         }         n=n/10;     }     if(n)     {         printf("Not Unique Number");     }     else     {         printf("Unique Number");     } }

---
+++ Unique_number.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100]={0},d;
+    scanf("%d",&n);
+    while(n)
+    {
+        d=n%10;
+        if(arr[d]==1)
+        {
+            break;
+        }
+        else
+        {
+            arr[d]=1;
+        }
+        n=n/10;
+    }
+    if(n)
+    {
+        printf("Not Unique Number");
+    }
+    else
+    {
+        printf("Unique Number");
+    }    
+}
