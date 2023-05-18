5323542723c6640c536fe99fe8a5170e83846614
nandana2005 <119730215+nandana2005@users.noreply.github.com>
2023-05-18 15:48:46 +0530

    # include <stdio.h> # include <stdlib.h> int main(){     int principal, rate, time, interest;     scanf("%d", &principal);     scanf("%d", &rate);     scanf("%d", &time);     interest = principal * rate * time / 100;     printf("%d", interest);     return 0;
    }

--- SIMPLE_INTEREST.c
+++ SIMPLE_INTEREST.c
@@ -1,9 +1,12 @@
-#include<stdio.h>
-int main()
-{
-    int P , R , T , SI ;
-    scanf("%d%d%d",&P,&R,&T);
-    SI = (P*R*T)/100;
-    printf("%d", SI);
+# include <stdio.h>
+# include <stdlib.h>
+int main(){
+    int principal, rate, time, interest;
+    scanf("%d", &principal);
+    scanf("%d", &rate);
+    scanf("%d", &time);
+    interest = principal * rate * time / 100;
+    printf("%d", interest);
+    return 0;
+
 }
-    
