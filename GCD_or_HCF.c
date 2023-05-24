00e431e20b708d131ac2864d442985d8a190e35f
nandana2005 <119730215+nandana2005@users.noreply.github.com>
2023-05-24 12:47:41 +0530

    #include<stdio.h> int main(){     int num1,num2,gcd,count=1;     scanf("%d%d",&num1,&num2);     while(count<=num1 && count<=num2){         if(num1%count==0 && num2%count==0){             gcd=count;         }         count++;     }     printf("%d ",gcd); }

--- GCD_or_HCF.c
+++ GCD_or_HCF.c
@@ -1,16 +1,13 @@
 #include<stdio.h>
-int main()
-{
-int num1,num2,gcd,count=1;
-scanf("%d%d",&num1,&num2);
-while(count<=num1&&count<=num2)
-{
-if(num1%count==0&&num2%count==0)
-{
-gcd=count;
-}
-count++;
-}
-printf("%d
+int main(){
+    int num1,num2,gcd,count=1;
+    scanf("%d%d",&num1,&num2);
+    while(count<=num1 && count<=num2){
+        if(num1%count==0 && num2%count==0){
+            gcd=count;
+        }
+        count++;
+    }
+    printf("%d
 ",gcd);
 }
