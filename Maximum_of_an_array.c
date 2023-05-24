56e53654fd2e0ffed2fba21cd6ba178b3ae56524
nandana2005 <119730215+nandana2005@users.noreply.github.com>
2023-05-24 19:46:21 +0530

    #include <stdio.h> int main() {   int n;   int arr[100];   scanf("%d", &n);
      for (int i = 0; i < n; ++i) {
        scanf("%d", &arr[i]);
      }
    
      // storing the largest number to arr[0]
      for (int i = 1; i < n; ++i) {
        if (arr[0] < arr[i]) {
          arr[0] = arr[i];
        }
      }
    
      printf("%d", arr[0]);
    
      return 0;
    }

---
+++ Maximum_of_an_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+  int n;
+  int arr[100];
+  scanf("%d", &n);
+
+  for (int i = 0; i < n; ++i) {
+    scanf("%d", &arr[i]);
+  }
+
+  // storing the largest number to arr[0]
+  for (int i = 1; i < n; ++i) {
+    if (arr[0] < arr[i]) {
+      arr[0] = arr[i];
+    }
+  }
+
+  printf("%d", arr[0]);
+
+  return 0;
+}
