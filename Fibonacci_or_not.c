0a947dc5d9cd51ddc13936b1205120457ef9f319
nandana2005 <119730215+nandana2005@users.noreply.github.com>
2023-05-24 12:54:14 +0530

    #include <stdio.h>   int main() {         int data, num1 = 0, num2 = 1, temp, flag = 0;
            scanf("%d", &data);
    
            /* 0 and 1 are fibonacci numbers */
            if (data == num1 || data == num2) {
                    printf("True", data);
                    return 0;
            }
    
            /* checking whether a given number is fobonacci no or not */
            while (num2 <= data) {
                    temp = num2;
                    num2 = num1 + num2;
                    num1 = temp;
                    if (num2 == data) {
                            flag = 1;
                            break;
                    }
            }
    
            /* print the results */
            if (flag) {
                    printf("True", data);
            } else {
                    printf("False", data);
            }
            return 0;
      }

--- Fibonacci_or_not.c
+++ Fibonacci_or_not.c
@@ -1,26 +1,31 @@
- #include<stdio.h>
-int main()
-{
-int a,b,c,next,num;
-scanf("%d", &num);
-if((num==0)||(num==1))
-   printf("
-%d is a Fibonacci term",num);
-else
-{
-   a=0;
-   b=1;
-   c=a+b;
-   while(c<num)
-   {
-     a=b;
-     b=c;
-     c=a+b;
-   }
-   if(c==num)
-     printf("True");
-   else
-     printf("False");
-}
-}
+#include <stdio.h>
+  int main() {
+        int data, num1 = 0, num2 = 1, temp, flag = 0;
 
+        scanf("%d", &data);
+
+        /* 0 and 1 are fibonacci numbers */
+        if (data == num1 || data == num2) {
+                printf("True", data);
+                return 0;
+        }
+
+        /* checking whether a given number is fobonacci no or not */
+        while (num2 <= data) {
+                temp = num2;
+                num2 = num1 + num2;
+                num1 = temp;
+                if (num2 == data) {
+                        flag = 1;
+                        break;
+                }
+        }
+
+        /* print the results */
+        if (flag) {
+                printf("True", data);
+        } else {
+                printf("False", data);
+        }
+        return 0;
+  }
