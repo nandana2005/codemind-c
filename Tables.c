d5c3d44ad774e8462b5bdedbc12da90e0e064cba
nandana2005 <119730215+nandana2005@users.noreply.github.com>
2023-06-01 13:16:00 +0530

    #include<stdio.h> int main(){     int a,x,i;     scanf("%d%d",&a,&x);     for(i=1;i<=x;i++){         if(i%2!=0)         printf("%d x %d = %d ",a,i,a*i);     } }

--- Tables.c
+++ Tables.c
@@ -1,10 +1,8 @@
 #include<stdio.h>
-int main()
-{
+int main(){
     int a,x,i;
     scanf("%d%d",&a,&x);
-    for(i=1;i<=x;i++)
-    {
+    for(i=1;i<=x;i++){
         if(i%2!=0)
         printf("%d x %d = %d
 ",a,i,a*i);
