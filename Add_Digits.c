419eb1f0a7f26b057f2fc79d574f85a78c036fa6
nandana2005 <119730215+nandana2005@users.noreply.github.com>
2023-06-06 20:29:55 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,s;     scanf("%d",&n);     s=n%9;     printf("%d",s); }

--- Add_Digits.c
+++ Add_Digits.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
-    int n;
+    int n,s;
     scanf("%d",&n);
-    if(n<=9)printf("%d",n);
-    else if(n%9==0)printf("9");
-    else printf("%d",n%9);
+    s=n%9;
+    printf("%d",s);
 }
